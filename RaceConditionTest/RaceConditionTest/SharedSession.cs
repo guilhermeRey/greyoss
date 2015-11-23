4b4a1d3646fa11dc9f174b39f7dc135b430bb369
Guilherme Rey <poli.rey@gmail.com>
2015-11-23 18:14:21 -0200

    Race Condition - C# Tests

---
+++ RaceConditionTest/RaceConditionTest/SharedSession.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RaceConditionTest
+{
+    public static class SharedSession
+    {
+        private static readonly object lockRunning = new object();
+        private static readonly object lockData = new object();
+
+        private static bool _running = false;
+        private static string _data = "";
+
+        public static bool Running
+        {
+            get
+            {
+                lock (lockRunning)
+                {
+                    return _running;
+                }
+            }
+
+            set
+            {
+                lock (lockRunning)
+                {
+                    _running = value;
+                }
+            }
+        }
+
+        public static void SetData(int seconds, string value)
+        {
+            lock (lockData)
+            {
+                int count = RaceCondition.Counter;
+                string aux = Thread.CurrentThread.Name;
+                RaceConditionTest.RaceCondition.threads[Thread.CurrentThread.Name].Status = "Running";
+                SharedSession._data = value;
+                RaceCondition.theForm.WriteLine(count + ". Thread " + aux + " wrote");
+                System.Threading.Thread.Sleep(seconds * 1000);
+            }
+
+            RaceConditionTest.RaceCondition.threads[Thread.CurrentThread.Name].Status = "";
+        }
+
+        public static string GetData(int seconds)
+        {
+            string _r = "";
+            lock (lockData)
+            {
+                int count = RaceCondition.Counter;
+                string aux = Thread.CurrentThread.Name;
+                RaceConditionTest.RaceCondition.threads[Thread.CurrentThread.Name].Status = "Running";
+                System.Threading.Thread.Sleep(seconds * 1000);
+                RaceCondition.theForm.WriteLine(count + ". Thread " + aux + " read");
+                _r = SharedSession._data;
+            }
+
+            RaceConditionTest.RaceCondition.threads[Thread.CurrentThread.Name].Status = "";
+            return _r;
+        }
+    }
+}
---
+++ RaceConditionTest/RaceConditionTest/ThreadMonitor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RaceConditionTest
+{
+    public partial class ThreadMonitor : Form
+    {
+        public ThreadMonitor(Dictionary<string, MyThread> threads)
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (RaceConditionTest.RaceCondition.threads != null)
+            {
+                string ts = "";
+
+                RaceConditionTest.RaceCondition.semThreadList.WaitOne();
+                foreach (KeyValuePair<string, MyThread> t in RaceConditionTest.RaceCondition.threads)
+                    ts += t.Value.TheThread.Name + "[" + t.Value.Status + "]: " + t.Value.TheThread.ThreadState.ToString() + "\r\n";
+                RaceConditionTest.RaceCondition.semThreadList.ReleaseMutex();
+                txtConsole.Text = ts;
+            }
+        }
+
+
+    }
+}
